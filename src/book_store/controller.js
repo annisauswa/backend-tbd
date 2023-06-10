eb393954e44d8481b827ac253bd1115cad70ddeb
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-06-10 23:57:19 +0700

    feat: update controller and queries

--- src/book_store/controller.js
+++ src/book_store/controller.js
@@ -1,12 +1,27 @@
 const pool = require('../../db');
 const queries = require('./queries');
 
-const getBook = (req, res) => {
-    pool.query(queries.getBook, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+const getBook = async(req, res) => {
+    try{
+        await pool.query("BEGIN");
+        pool.query(queries.getBook, (error, results) => {
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        });
+        await pool.query("COMMIT");
+    }catch(err){
+        await pool.query("ROLLBACK");
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 };
+
+// const getBook = (req, res) => {
+//     pool.query(queries.getBook, (error, results) => {
+//         if (error) throw error;
+//         res.status(200).json(results.rows);
+//     });
+// };
 
 const getBookById = (req, res) => {
     const id = parseInt(req.params.id);
@@ -31,19 +46,20 @@ const addBook = (req, res) => {
 
 const updateBook = (req, res) => {
     const id = parseInt(req.params.id);
-    const { title, auth_id, publication_year, pages, price, pub_id, last_update } = req.body;
+    const { title, publication_year, pages, price} = req.body;
     pool.query(queries.getBookById, [id], (error, results) => {
         const noBookFound = !results.rows.length;
         if (noBookFound){
             res.send("Book doesn't exist in the database.")
         }
 
-        pool.query(queries.updateBook, [title, auth_id, publication_year, pages, price, pub_id, last_update, id], (error, results) => {
+        pool.query(queries.updateBook, [title, publication_year, pages, price, id], (error, results) => {
             if (error) throw error;
             res.status(200).send(`Book updated with ID: ${id}`);
         });
     });
 };
+
 
 const deleteBook = (req, res) => {
     const id = parseInt(req.params.id);
@@ -59,10 +75,51 @@ const deleteBook = (req, res) => {
     });
 };
 
+const getStore = (req, res) => {
+    pool.query(queries.getStore, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+const getStaff = (req, res) => {
+    pool.query(queries.getStaff, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+const getOrder = (req, res) => {
+    pool.query(queries.getOrder, (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+const sqlBuilder = async(req, res) => {
+    try{
+        await pool.query("BEGIN");
+        const { queries } = req.body;
+        pool.query(queries, (error, results) => {   
+            if (error) throw error;
+            res.status(200).json(results.rows);
+        });
+        await pool.query("COMMIT");
+    } catch (error) {
+        await pool.query("ROLLBACK");
+        console.error('Error executing queries:', error);
+        res.status(500).json({ error: 'An error occurred while executing the queries.' });
+    }
+};
+
 module.exports = {
     getBook,
     getBookById,
     addBook,
     deleteBook,
     updateBook,
+    getStore,
+    getStaff,
+    getOrder,
+    sqlBuilder
 };
--- src/book_store/queries.js
+++ src/book_store/queries.js
@@ -3,7 +3,11 @@ const getBookById = "SELECT * FROM book WHERE id = $1";
 const checkTitleExists = 'SELECT * FROM book s WHERE s.title = $1'
 const addBook = 'INSERT INTO book (id, title, auth_id, publication_year, pages, price, pub_id, last_update) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)'
 const deleteBook = 'DELETE FROM book WHERE id = $1';
-const updateBook = 'UPDATE book SET title = $1, auth_id = $2, publication_year = $3, pages = $4, price = $5, pub_id = $6, last_update = $7 WHERE id = $8';
+const updateBook = 'UPDATE book SET title = $1, publication_year = $2, pages = $3, price = $4 WHERE id = $5';
+
+// const getStore = "SELECT * FROM store";
+// const getStaff = "SELECT * FROM staff";
+// const getOrder = "SELECT * FROM book_order";
 
 module.exports = {
     getBook,
@@ -12,4 +16,7 @@ module.exports = {
     addBook,
     deleteBook,
     updateBook,
+    // getStore,
+    // getStaff,
+    // getOrder
 };
