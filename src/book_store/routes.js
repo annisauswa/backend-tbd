43a3d85d01be30935a92a41ad6b5b408930c4b99
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-06-29 10:23:24 +0700

    feat: router put to patch

--- src/book_store/routes.js
+++ src/book_store/routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/', controller.getBook);
 router.post('/', controller.addBook);
 router.get('/:id', controller.getBookById);
-router.put('/:id', controller.updateBook);
+router.patch('/:id', controller.updateBook);
 router.delete('/:id', controller.deleteBook);
 
 module.exports = router;
